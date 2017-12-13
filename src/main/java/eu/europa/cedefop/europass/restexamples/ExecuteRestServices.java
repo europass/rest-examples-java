8b392bd594f9be7bcbcdec5333601025536420f3
jos <jos@eworxsa.com>
2017-12-13 18:55:37 +0200

    #EPAS-1088 More cleanup & refactoring

--- src/main/java/eu/europa/cedefop/europass/restexamples/ExecuteRestServices.java
+++ src/main/java/eu/europa/cedefop/europass/restexamples/ExecuteRestServices.java
@@ -1,12 +1,17 @@
 package eu.europa.cedefop.europass.restexamples;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ExecuteRestServices {
 
     private static final String DEFAULT_LANGUAGE = "en";
-    private static final String DEFAULT_REST_URL = "https://staging.europass.eworx.gr/rest";
+    private static final String DEFAULT_REST_URL = "https://europass.cedefop.europa.eu/rest";
+    private static Map restActionMap = new HashMap<String, String>();
 
     public static void main(String[] args) throws Exception {
 
+        initMapRestActionsArray();
         final String type = System.getProperty("type");
         final String inputFile = System.getProperty("inputPathFile");
         final String outputFile = System.getProperty("outputPathFile");
@@ -20,7 +25,6 @@ public class ExecuteRestServices {
         if (baseRestUrl == null) {
             baseRestUrl = DEFAULT_REST_URL;
         }
-
         throwExceptionWhenEmptyArgs(inputFile, outputFile, type);
 
         final WebServicesRequests webServicesRequests = new WebServicesRequests();
@@ -43,9 +47,28 @@ public class ExecuteRestServices {
         if (outputFile == null) {
             throw new Exception("Output path file not provided");
         }
-        if (type == null) {
-            throw new Exception("Rest action not provided");
+        if (type == null || !restActionMap.containsKey(type)) {
+            throw new Exception("Rest action not provided/supported");
         }
     }
 
+    private static void initMapRestActionsArray() {
+        restActionMap.put("xml-to-pdf-cv-esp", "/v1/document/to/pdf");
+        restActionMap.put("xml-to-pdf-esp-only", "/v1/document/to/pdf-esp");
+        restActionMap.put("xml-to-pdf-cv-only", "/v1/document/to/pdf-cv");
+        restActionMap.put("xml-to-doc", "/v1/document/to/word");
+        restActionMap.put("xml-to-odt", "/v1/document/to/opendoc");
+        restActionMap.put("xml-to-json", "/v1/document/to/json");
+        restActionMap.put("xml-upgrade", "/v1/document/upgrade");
+        restActionMap.put("extract-xml", "/v1/document/extraction");
+
+        restActionMap.put("json-to-pdf-cv-esp", "/v1/document/to/pdf");
+        restActionMap.put("json-to-pdf-esp-only", "/v1/document/to/pdf-esp");
+        restActionMap.put("json-to-pdf-cv-only", "/v1/document/to/pdf-cv");
+        restActionMap.put("json-to-doc", "/v1/document/to/word");
+        restActionMap.put("json-to-odt", "/v1/document/to/opendoc");
+        restActionMap.put("json-to-xml-cv", "/v1/document/to/xml-cv");
+        restActionMap.put("json-to-xml-esp", "/v1/document/to/xml-esp");
+        restActionMap.put("json-to-xml", "/v1/document/to/xml");
+    }
 }
--- src/main/java/eu/europa/cedefop/europass/restexamples/WebServicesRequests.java
+++ src/main/java/eu/europa/cedefop/europass/restexamples/WebServicesRequests.java
@@ -14,10 +14,10 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 import java.io.*;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
-import java.util.HashMap;
-import java.util.Map;
 
 public class WebServicesRequests {
 
@@ -26,53 +26,14 @@ public class WebServicesRequests {
     private String outputFile;
     private String type;
     private String url;
-    private Map restActionMap = new HashMap<String, String>();
-
-    public WebServicesRequests() {
-        mapRestActionsWithArgType();
-    }
-
-    private void mapRestActionsWithArgType() {
-        restActionMap.put("xml-to-pdf-cv-esp", "/v1/document/to/pdf");
-        restActionMap.put("xml-to-pdf-esp-only", "/v1/document/to/pdf-esp");
-        restActionMap.put("xml-to-pdf-cv-only", "/v1/document/to/pdf-cv");
-        restActionMap.put("xml-to-doc", "/v1/document/to/word");
-        restActionMap.put("xml-to-odt", "/v1/document/to/opendoc");
-        restActionMap.put("xml-to-json", "/v1/document/to/json");
-        restActionMap.put("xml-upgrade", "/v1/document/upgrade");
-        restActionMap.put("extract-xml", "/v1/document/extraction");
-
-        restActionMap.put("json-to-pdf-cv-esp", "/v1/document/to/pdf");
-        restActionMap.put("json-to-pdf-esp-only", "/v1/document/to/pdf-esp");
-        restActionMap.put("json-to-pdf-cv-only", "/v1/document/to/pdf-cv");
-        restActionMap.put("json-to-doc", "/v1/document/to/word");
-        restActionMap.put("json-to-odt", "/v1/document/to/opendoc");
-        restActionMap.put("json-to-xml-cv", "/v1/document/to/xml-cv");
-        restActionMap.put("json-to-xml-esp", "/v1/document/to/xml-esp");
-        restActionMap.put("json-to-xml", "/v1/document/to/xml");
-    }
 
     public void execute() {
 
         try {
-
-            final SSLContext sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null,
-                new TrustManager[]{ new X509TrustManager() {
-                    public X509Certificate[] getAcceptedIssuers() { return null; }
-                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
-                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
-                }}, new SecureRandom());
-
-            final SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
-
             final InputStream is = new FileInputStream(new File(this.inputFile));
-            final HttpClient client = HttpClientBuilder.create().setSSLSocketFactory(socketFactory).build();
+            final HttpClient client = HttpClientBuilder.create().setSSLSocketFactory(createSSLConnectionFactory()).build();
 
-            if (getRestActionByType(type) == null) {
-                throw new Exception("Not supported REST action");
-            }
-            final HttpPost postRequest = new HttpPost(this.url + getRestActionByType(this.type));
+            final HttpPost postRequest = new HttpPost(this.url + this.type);
             final HttpEntity entity = new ByteArrayEntity(IOUtils.toByteArray(is));
 
             postRequest.setEntity(entity);
@@ -87,8 +48,16 @@ public class WebServicesRequests {
         }
     }
 
-    private String getRestActionByType(final String type) {
-        return (String)restActionMap.get(type);
+    private SSLConnectionSocketFactory createSSLConnectionFactory() throws KeyManagementException, NoSuchAlgorithmException {
+        final SSLContext sslContext = SSLContext.getInstance("SSL");
+        sslContext.init(null,
+                new TrustManager[]{ new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() { return null; }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                }}, new SecureRandom());
+
+        return new SSLConnectionSocketFactory(sslContext);
     }
 
     public void setInputFile(final String inputFile) {
